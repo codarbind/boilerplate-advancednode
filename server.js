a46dcd81b92b3d0509b74f0ac0cabb7836995683
Abdwahab Adekola <adekolaabdwahababisoye@gmail.com>
2024-11-25 03:06:38 +0100

    local strategy+3a

--- server.js
+++ server.js
@@ -23,11 +23,7 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+
 
 
 myDB(async client => {
@@ -84,7 +80,11 @@ passport.use(new LocalStrategy((username, password, done) => {
     return done(null, user);
   });
 }));
-
+app.use((req, res, next) => {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database',showLogin: true });
